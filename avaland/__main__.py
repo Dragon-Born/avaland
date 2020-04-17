e623d508cd1a3c2a13fd377c5c8a0cda95b12396
Arian Amiramjadi <me@arian.lol>
2020-04-18 02:22:58 +0430

    just a test

--- avaland/__main__.py
+++ avaland/__main__.py
@@ -3,7 +3,7 @@ from avaland.sources import *
 import argparse
 
 parser = argparse.ArgumentParser(description='Avaland Music Downloader')
-parser.add_argument("query", type=str,
+parser.add_argument("--query", type=str,
                     help='an integer for the accumulator')
 
 args = parser.parse_args()
