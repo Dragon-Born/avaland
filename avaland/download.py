85dd707dbcce151d2f8b9e5852300872f7e97f3c
Arian Amiramjadi <me@arian.lol>
2020-04-18 12:46:45 +0430

    add more sources

--- avaland/download.py
+++ avaland/download.py
@@ -50,7 +50,7 @@ class Download:
                     f.write(data)
                     done = int(100 * dl / total_length)
                     percent = " " + str(done) if done < 10 else done
-                    sys.stdout.write("\r%s%%|%s%s| (%s / %s) %s/s" % (
+                    sys.stdout.write("\r%s%%|%s%s| (%s / %s) %s/s\033[K" % (
                         percent, "█" * int(done // 2), " " * (int((50 - done // 2))), convert_size(dl),
                         convert_size(total_length), convert_size(int(dl // (time.time() - start))).replace(" ", "")))
                     sys.stdout.flush()
