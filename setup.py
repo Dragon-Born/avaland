befb79833013e4afff0c013d5299ce8699fcd7d0
S. Ramezani <umbrpro@gmail.com>
2020-04-19 12:56:09 +0430

    Refactor setup and remove idle comment

--- setup.py
+++ setup.py
@@ -1,36 +1,36 @@
 from setuptools import setup, find_packages
 
-setup(name='Avaland Music Downloader',
-      version='0.0.1',
-      description='A simple cli to get WARP+ as WireGuard configuration',
-      url='https://github.com/dragon-born/avaland',
-      download_url='https://github.com/warp-plus/warpy-python/archive/v_05.tar.gz',
-      keywords=['avaland', 'persian music downloader python', 'persian music api', 'farsi music downloader',
-                "iranian music", "iranian music downloader", "farsi music API", ""],
-      license='mit',
-      author='Arian Amiramjadi',
-      author_email='me@arian.lol',
-      packages=find_packages(),
-      install_requires=["requests", 'pathlib2', "typing"],
-      scripts=['bin/avaland'],
-      # entry_points={'console_scripts': [
-      #     'avaland = avaland:main',
-      # ]},
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-          'Intended Audience :: Developers',  # Define that your audience are developers
-          'Environment :: Console',
-          'Topic :: Software Development :: Build Tools',
-          'License :: OSI Approved :: mit License',  # Again, pick a license
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-      ],
-      )
+setup(
+    name='Avaland Music Downloader',
+    version='0.0.1',
+    description='Avaland is the best free music downloader to download music free online.',
+    url='https://git.arian.lol/arian/avaland',
+    keywords=[
+        'Avaland',
+        'Persian music', 'Persian music downloader', 'Persian music api',
+        'Iranian music', 'Iranian music downloader', 'Iranian music api',
+    ],
+    license='MIT',
+    author='Arian Amiramjadi',
+    author_email='me@arian.lol',
+    packages=find_packages(),
+    install_requires=['requests', 'pathlib2', 'typing'],
+    scripts=['bin/avaland'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Chose either '3 - Alpha', '4 - Beta' or '5 - Production/Stable' as the current state of your package
+        'Intended Audience :: Developers',  # Define that your audience are developers
+        'Environment :: Console',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',  # Again, pick a license
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
